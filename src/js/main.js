4c961fe967bbcb7c3cc44690b556fe039d47443c
AlexeyPogorelov <hirurg555@gmail.com>
2016-03-06 23:02:51 +0200

    just commit

--- src/js/main.js
+++ src/js/main.js
@@ -8,9 +8,13 @@ var animationPrefix = (function () {
 				"WebkitTransition": "webkitAnimationEnd"
 			};
 			for (t in transitions){
+
 				if (el.style[t] !== undefined){
+
 					return transitions[t];
+
 				}
+
 			}
 		})(),
 		loading = {
@@ -19,20 +23,29 @@ var animationPrefix = (function () {
 			state: 0,
 			preloader: $('body > .preloader'),
 			loaded: function () {
+
 				if(++loading.state == loading.trg) {
+
 					loading.status(1);
 					setTimeout(loading.done, 500);
+
 				} else {
+
 					loading.status(loading.state / loading.trg / 1.1);
+
 				}
 			},
 			status: function (mult) {
+
 				loading.preloader.find('> .after').css({
 					'width': mult * 100 + '%'
 				});
+
 			},
 			done: function () {
+
 				if (loading.finished) {
+
 					return;
 				}
 
@@ -40,29 +53,40 @@ var animationPrefix = (function () {
 				loading.preloader.animate({}).delay(100).animate({
 					'opacity': 0
 				}, 600, function () {
+
 					loading.status(0);
 					$(this).detach();
 					loading.finished = true;
+
 				});
+
 			}
 	};
 
 	// TODO
 	$('img').each(function () {
+
 		if (!this.naturalWidth || true) {
+
 			loading.trg ++;
 			$(this).one('load', loading.loaded);
+
 		}
+
 	});
 
 setTimeout(function () {
+
 	loading.status(1);
 	setTimeout(loading.done, 500);
+
 }, 10000);
 
 $(window).on('load', function () {
+
 	loading.status(1);
 	setTimeout(loading.done, 500);
+
 });
 
 $(document).on('ready', function () {
@@ -73,51 +97,80 @@ $(document).on('ready', function () {
 		},
 		bodyOverflow = {
 			fixBody: function () {
+
 				$('body').width($('body').width())
 					.addClass('fixed');
+
 			},
 			unfixBody: function () {
+
 				$('body')
 					.css({
 						'width': 'auto'
 					})
 					.removeClass('fixed');
+
 			},
 			resize: function () {
+
 				this.unfixBody();
+
 			}.bind(this)
+
 		},
 		goUp = (function () {
+
 			var $el = $('#to-top'),
 				state = false,
 				speed = 900,
 				paused = false;
 			var plg = {
 				up: function () {
+
 					paused = true;
+
 					$("html, body").stop().animate({scrollTop:0}, speed, 'swing', function () {
+
 						paused = false;
+
 					});
+
 					plg.hide();
+
 				},
 				show: function () {
+
 					if (!state && !paused) {
+
 						$el.addClass('opened');
+
 						state = true;
+
 					}
+
 				},
 				hide: function () {
+
 					if (state) {
+
 						$el.removeClass('opened');
+
 						state = false;
+
 					}
+
 				},
 				$el: $el
 			};
+
 			$el.on('click', function () {
+
 				plg.up();
+
 			});
+
 			return plg;
+
 		})();
 
 		// modals
@@ -165,10 +218,15 @@ $(document).on('ready', function () {
 
 					// TODO test it
 					for (var i = 0; i < this.opened.length; i++) {
+
 						if (modal == this.opened[i].get(0)) {
+
 							this.opened.splice(i, 1);
+
 							break;
+
 						}
+
 					}
 
 				} else {
@@ -216,45 +274,88 @@ $(document).on('ready', function () {
 
 				if ($target.length) {
 
-					$target.removeClass('opened');
+					modals.closeModal( $target );
 
 				}
 
 			} else {
 
-				$self.closest('.opened').removeClass('opened');
+				modals.closeModal( $self.closest('.opened') );
 
 			}
 
 		});
 
 		$('.modal-holder').on('click', function (e) {
+
 			if (e.target === this) {
+
 				modals.closeModal( $(this) );
+
 			}
+
 		});
 
 		$(window).on('keyup', function (e) {
+
 			// esc pressed
 			if (e.keyCode == '27') {
 
 				modals.closeModal();
 
 			}
+
 		});
 
 		// add content button
+		var createContent = function (type) {
+
+			if ( typeof createContent[type] === 'function') {
+
+				var $editor = createContent[type]();
+				this.before( $editor );
+
+			} else {
+
+				console.warn('К сожалению, тип ' + type + ' не предусмотрен программой');
+				console.log(this);
+
+			}
+
+		};
+
+		createContent.text = function () {
+
+			var $content = $('<div>')
+				.addClass('editor')
+				.attr('contenteditable', true);
+			// console.log('text');
+
+			return $content;
+
+		};
+
+		createContent.table = function() {
+
+			var $content = $('<div>')
+				.addClass('editor')
+				.attr('contenteditable', true);
+			// console.log('table');
+
+			return $content;
+
+		};
+
 		$('[data-add-content]').each(function () {
 
 			var $self = $(this);
 
 			$self.on('click', function (e) {
 
-				// element types
-				alert( e.target.getAttribute('type') );
-				e.target.getAttribute('type');
+				createContent.call($self, e.target.getAttribute('data-type') );
 
 			});
+
 		});
 
 		//scroll
